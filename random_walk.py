b125ac6e6f845a31c1d13f56bb3580f07a8be82e
Karthik Ramesan <136253498+fsydurden@users.noreply.github.com>
2025-07-06 13:06:36 +0530

    Update random_walk.py

--- random_walk.py
+++ random_walk.py
@@ -2,15 +2,27 @@ from turtle import Turtle, Screen
 import random
 
 pen = Turtle()
+turtle.colormode(255)
 pen.shape("arrow")
-colors = ["red", "blue", "green", "yellow", "orange", "purple", "cyan"]
+pen.speed("fastest")
+
+def colors():
+    r = random.randint(0,255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_colour = (r,g,b)
+    return random_colour
+
+
+
 directions = [0, 90, 180, 270]
 
 screen = Screen()
 screen.bgcolor("Black")
 
 for _ in range(200):
-    pen.color(random.choice(colors))
+    colours = colors()
+    pen.color(colours)
     pen.pensize(5)
     pen.forward(20)
     pen.setheading(random.choice(directions))
