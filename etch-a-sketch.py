473f31efd2f47efbadf329343977fa8080a500ea
Karthik Ramesan <136253498+fsydurden@users.noreply.github.com>
2025-07-07 21:14:07 +0530

    Update etch-a-sketch.py

--- etch-a-sketch.py
+++ etch-a-sketch.py
@@ -5,22 +5,27 @@ screen = Screen()
 
 def move_forward():
     pen.forward(10)
+    #function to move forward
 
 def move_backwards():
     pen.backward(10)
+    #function to move backward
 
 
 def clock_wise():
     pen.right(10)
+    #function to turn clockwise
 
 def anti_clockwise():
     pen.left(10)
+    #function to turn anti clockwise
 
 def clear_screen():
     pen.clear()
     pen.penup()
     pen.home()
     pen.pendown()
+    #function that clears the screen, penup is used because it will draw a line backwardif not used.
 
 
 screen.listen()
